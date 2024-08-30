c8e7aed92bdf91584edd9719116f5c5ea4cb2b48
Jhomar rosaliere <Jhomarrosaliere@icloud.com>
2024-08-30 12:35:11 -0400

    Add files via upload

---
+++ 01_03_infnite_skills.py
@@ -0,0 +1,8 @@
+
+ # I found an text editor tool_that was very helpful to use, when you make any spelling mistakes when codeing 
+
+ # Docker is an other resouce application that help's with Running other applions with it but 
+ # Also mangeing codeing as well, with faster development process 
+
+ # and if I have any thing to Recommending for new comer's to python is that Docker is one of the best application software to use if you want to have an organized progamming space.
+  
