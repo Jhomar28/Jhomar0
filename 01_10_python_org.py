16de55b24940262a58314d72478c42f63df6a9bc
Jhomar Rosaliere <Jhomarrosaliere@icloud.com>
2024-09-04 09:47:39 -0400

    Update 01_10_python_org.py

--- 01_10_python_org.py
+++ 01_10_python_org.py
@@ -1,3 +1,3 @@
  # (one of the resource's that I looked at is just some pretty denct resource use with python 
- #and I think they would be useful if you want some codeing done Faster)
+ # and I think they would be useful if you want some codeing done Faster)
  
