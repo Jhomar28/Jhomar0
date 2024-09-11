76137bb2f8c0e0bf5454094ad59adc9eed5c618e
Jhomar Rosaliere <Jhomarrosaliere@icloud.com>
2024-09-11 09:38:37 -0400

    Add files via upload

---
+++ 2-11 adding comments.py
@@ -0,0 +1,17 @@
+# This is a single-line comment
+print("Hello, world!")
+
+"""
+# This is a multi-line comment. you can write multiple lines of comments here.
+"""
+print("Hello again!")
+
+# Author: Jhomar Rosaliere
+# Date: september 11, 2024
+
+# This program creates a full name from a first and last name.
+
+first_name = "Jhomar"
+last_name = "Rosaliere"
+full_name = f"{Jhomar} {Rosaliere}"
+print(full_name)
---
+++ 2-9 Number eight.py
@@ -0,0 +1,16 @@
+print(2 + 3)  # Addition
+
+print(3 - 2)  # Subtraction
+
+print(2 * 3)  # Multiplication
+
+print(5 / 6)  # Division
+
+
+print(4 / 2)
+
+print(1 + 2.0)
+
+print(2 * 3.0)
+
+print(3.0 ** 2)
