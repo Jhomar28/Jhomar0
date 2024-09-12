397694cc49bfb448aec4995213bd9037ec74e6fc
Jhomar Rosaliere <Jhomarrosaliere@icloud.com>
2024-09-12 08:47:03 -0400

    Add files via upload

---
+++ bicyles  3.py
@@ -0,0 +1,16 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+
+print(bicycles[0])
+
+print(bicycles[0].title())
+
+print(bicycles[1])
+print(bicycles[2])
+
+print(bicycles[-1])
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+message = f"My first bicycles was a {bicycles[0].title()}."
+
+print(message)
