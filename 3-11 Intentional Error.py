85485a34747c7945cef76ae97d4be930db732878
Jhomar Rosaliere <Jhomarrosaliere@icloud.com>
2024-09-13 00:44:36 -0400

    Add files via upload

---
+++ 3-11 Intentional Error.py
@@ -0,0 +1,7 @@
+quests = ["Albert Einstein", "Marie Curie", "Alan Turing", "Isaac newton", "Ada Lovelace", "Nikola Tesla"]
+
+# Try to access the 10th element (which doesn't exist)
+print(quests[10])
+
+
+Print(quests[0]) # this will print "albert Einstein"
