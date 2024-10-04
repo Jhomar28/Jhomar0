e241c68591d0d175f8f7c9d7b54cdc5c39323bd0
Jhomar Rosaliere <Jhomarrosaliere@icloud.com>
2024-10-04 14:18:47 -0400

    Add files via upload

---
+++ Dictionaries/6_1_person.py
@@ -0,0 +1,11 @@
+person = { 
+    'first_name' : 'eric',
+    'Last_name' : 'matthes',
+    'age' : 43, 
+    'city' : 'sitka',
+}
+
+print(person['first_name'])
+print(person['Last_name'])
+print(person['age'])
+print(person['city'])
