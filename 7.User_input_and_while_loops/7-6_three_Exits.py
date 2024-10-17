d7395ac99a5b2b3fc49a3b39a178a39d49c9d40d
Jhomar Rosaliere <Jhomarrosaliere@icloud.com>
2024-10-17 07:53:44 -0400

    Add files via upload

---
+++ 7.User_input_and_while_loops/7-6_three_Exits.py
@@ -0,0 +1,11 @@
+while True:
+    user_input = input ("Enter a number (or quit to exit): ")
+ 
+    if user_input == "quit":
+        break
+
+    number = int(user_input)
+    print(number * 5)
+
+    
+
