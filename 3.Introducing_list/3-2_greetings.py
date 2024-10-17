1bb0fbea8c7c113ddff2952e104b6675646c721a
Jhomar Rosaliere <Jhomarrosaliere@icloud.com>
2024-10-17 07:51:50 -0400

    Add files via upload

---
+++ 3.Introducing_list/3-2_greetings.py
@@ -0,0 +1,4 @@
+names = ["Alice", "Bob", "Charlie"]
+
+for name in names:Message = f"Hello, {name}! How are you?"
+print(Message)
---
+++ 3.Introducing_list/3-3_Your_own_list 4.py
@@ -0,0 +1,3 @@
+favorite_Pokemon = ["Gliscor", "Infernape", "Dewott", "Pikachu"]
+
+for Pokemon in favorite_Pokemon:print(f"I would love to own a {Pokemon}.")
