9509b71990d5980d3001823f85fc802a25220731
Jhomar Rosaliere <Jhomarrosaliere@icloud.com>
2024-10-17 07:50:30 -0400

    Delete If Statements directory

--- If Statements/5-4_Alien colors_#2.py
+++
@@ -1,6 +0,0 @@
-alien_color =  'Yellow'
-
-if alien_color == 'green':
-    print("you just earned 5 points!")
-else:
-    print("you just earned 10 points")
--- If Statements/5-5_Alien_Colors_#3.py
+++
@@ -1,8 +0,0 @@
-alien_color = 'red'
-
-if alien_color == 'green':
-    print("you just earned 5 points!")
-elif alien_color == 'yellow':
-    print("you just earned 10 points!")
-else:
-    print("you just earned 15 points!")
